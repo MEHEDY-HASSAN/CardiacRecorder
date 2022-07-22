b253bf2ab6804878abc25a4e0302786bd3f82025
MD.Mehedi Hasan Emon <65850447+scorpius72@users.noreply.github.com>
2022-07-23 02:05:42 +0600

    record design and recycler view has been added

--- app/src/main/java/com/example/cardiacmonitor/HomeAcitivity.java
+++ app/src/main/java/com/example/cardiacmonitor/HomeAcitivity.java
@@ -1,15 +1,31 @@
 package com.example.cardiacmonitor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.icu.text.Collator;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class HomeAcitivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    DatabaseReference database;
+    MyAdapter myAdapter;
+    ArrayList<Record> list;
 
     public Button logout;
     private FirebaseAuth mAuth;
@@ -32,6 +48,31 @@ public class HomeAcitivity extends AppCompatActivity {
     private void initialization() {
         mAuth = FirebaseAuth.getInstance();
         logout = (Button) findViewById(R.id.home_logOut);
+        recyclerView = (RecyclerView) findViewById(R.id.record_recycler_view);
+        database = FirebaseDatabase.getInstance().getReference("Record");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        list = new ArrayList<>();
+        myAdapter = new MyAdapter(this,list);
+        recyclerView.setAdapter(myAdapter);
+
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    Record record = dataSnapshot.getValue(Record.class);
+                    list.add(record);
+                }
+                myAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
     private void mainActivityIntent() {
         Intent mainIntent = new Intent(this,MainActivity.class);
