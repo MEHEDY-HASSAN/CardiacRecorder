9ad4a1fd42981f672fff15dac1a641e09258c8d3
MD.Mehedi Hasan Emon <65850447+scorpius72@users.noreply.github.com>
2022-07-28 18:25:43 +0600

    all done

--- app/src/androidTest/java/com/example/cardiacmonitor/HomeActivityTest.java
+++ app/src/androidTest/java/com/example/cardiacmonitor/HomeActivityTest.java
@@ -1,14 +1,6 @@
 package com.example.cardiacmonitor;
 
 
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.LargeTest;
-
-import org.junit.runner.RunWith;
-
-@RunWith(AndroidJUnit4.class)
-@LargeTest
 public class HomeActivityTest {
-
 
 }
